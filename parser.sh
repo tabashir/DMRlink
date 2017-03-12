0e26853a05e1eb0586f7b009778510f58d254773
Jez McKinley <jez@mckinley.biz>
2017-03-12 11:15:54 +0000

    JM: Registrations now in status page

--- parser.sh
+++ parser.sh
@@ -3,9 +3,10 @@
 while true; do
   ls -1t logs/dmr.log.* |head -n1 | xargs cat > webroot/current.log
   cat logs/dmr.log >> webroot/current.log
-  cat webroot/current.log |grep Regist | sort |uniq > webroot/system.log
+  cat webroot/current.log |grep Regist | sort |uniq > webroot/registrations.log
   cat webroot/current.log |grep --no-group-separator -A1 'RADIO ID' |grep --no-group-separator -B1 'IP Address' |sed 'N;s/\n/ /' |sort |uniq > webroot/dmrs.log
+  tail -n 50 webroot/current.log > webroot/system.log
 
-  sleep 10
+  sleep 3
 
 done
