db4d64716e2b7e0a24eb5dd8cc057c890d3b871f
Jez McKinley <jez@mckinley.biz>
2017-03-12 10:58:22 +0000

    JM: dmr.log is now last so tail works correctly

--- parser.sh
+++ parser.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 while true; do
-
-  find logs -name dmr.* |xargs cat > webroot/current.log
+  ls -1t logs/dmr.log.* |head -n1 | xargs cat > webroot/current.log
+  cat logs/dmr.log >> webroot/current.log
   cat webroot/current.log |grep Regist | sort |uniq > webroot/system.log
   cat webroot/current.log |grep --no-group-separator -A1 'RADIO ID' |grep --no-group-separator -B1 'IP Address' |sed 'N;s/\n/ /' |sort |uniq > webroot/dmrs.log
 
